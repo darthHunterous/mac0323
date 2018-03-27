7b98fa9f0baf5c7c6fbe113461f87cef6afa51b0
Édio Cerati Neto <edioceratineto@gmail.com>
2018-03-27 11:26:35 -0300

    Finish Percolation.java

--- ep02/Percolation.java
+++ ep02/Percolation.java
@@ -1,7 +1,9 @@
+import edu.princeton.cs.algs4.*;
+
 public class Percolation {
     private int[][] grid;
     private int openSites, size;
-    private QuickFindUF uf;
+    private WeightedQuickUnionUF wquuf;
 
     public Percolation(int n) {
         if (n <= 0)
@@ -9,38 +11,43 @@ public class Percolation {
         grid = new int[n][n];
         openSites = 0;
         size = n;
-        uf = new QuickFindUF(n*n);
+        wquuf = new WeightedQuickUnionUF(n*n + 2);
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 grid[i][j] = -1;
     }
 
     public void open(int row, int col) {
-        if(row >= size || col >= size)
+        if(row >= size || col >= size || row < 0 || col < 0)
             throw new IllegalArgumentException();
         if (grid[row][col] == -1) {
-            if (row != 0)
-                grid[row][col] = 0;
-            else
-                grid[row][col] = 1;
             openSites++;
+            grid[row][col] = 0;
+            if (row == 0)
+                wquuf.union(row*size+col, size*size);
+            if (row == size-1)
+                wquuf.union(row*size+col, size*size+1);
             if (row != 0 && isOpen(row-1, col)) // tem vizinho acima
-                uf.union(row*size+col, (row-1)*size+col);
+                wquuf.union(row*size+col, (row-1)*size+col);
             if (row != size-1 && isOpen(row+1, col)) // tem vizinho abaixo
-                uf.union(row*size+col, (row+1)*size+col);
+                wquuf.union(row*size+col, (row+1)*size+col);
             if (col != 0 && isOpen(row, col-1)) // tem vizinho a esquerda
-                uf.union(row*size+col, row*size+(col-1));
+                wquuf.union(row*size+col, row*size+(col-1));
             if (col != size-1 && isOpen(row, col+1)) // tem vizinho a direita
-                uf.union(row*size+col, row*size+(col+1));
+                wquuf.union(row*size+col, row*size+(col+1));
         }
     }
 
     public boolean isOpen(int row, int col) {
+        if(row >= size || col >= size || row < 0 || col < 0)
+            throw new IllegalArgumentException();
         return (grid[row][col] == 0) || (grid[row][col] == 1);
     }
 
     public boolean isFull(int row, int col) {
-        return (grid[row][col] == 1);
+        if(row >= size || col >= size || row < 0 || col < 0)
+            throw new IllegalArgumentException();
+        return (wquuf.connected(wquuf.find(row*size+col), size*size));
     }
 
     public int numberOfOpenSites() {
@@ -48,12 +55,8 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        if (uf.connected(0, size*size-1))
+        if (wquuf.connected(size*size, size*size+1))
             return true;
         return false;
-    }
-
-    public static void main(String[] args) {
-
     }
 }
